4d380d49bdd92aa06e84645f863bd7af1140bb66
Costel <costelmartinescu2000@gmail.com>
2024-08-05 10:52:49 +0300

    implement swiper

--- client/src/components/CaruselMobile.jsx
+++ client/src/components/CaruselMobile.jsx
@@ -1,66 +1,69 @@
 import React from "react";
-import Slider from 'react-slick';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css/effect-fade';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/zoom';
+
+import { Zoom, Autoplay, EffectFade, Pagination } from 'swiper/modules';
 
 const CaruselMobile = (props) => {
 
-    const settings = {
-        arrows: false,
-        lazyLoad: false,
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 500,
-        autoplaySpeed: 5000,
-        cssEase: "linear"
-
-
-    };
-
     return (
 
-        <Slider   {...settings} className={props.theme.mod.bgHeader} >
-            <div>
-                <div>
-                    <picture >
-                        <img className=" mx-auto carusel-img img-fluid" src="/uploads/carusel--2.webp" alt="foto-nunta-natura" />
-                    </picture>
-                </div>
+        <Swiper
+            style={{
+                '--swiper-pagination-color': '#fff',
+            }}
+            spaceBetween={0}
+            effect={'fade'}
+            zoom={true}
+            modules={[Zoom, EffectFade, Pagination, Autoplay]}
+            pagination={{
+                clickable: true,
+            }}
+            autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            }}
+            slidesPerView={1}
+            className={props.theme.mod.bgHeader} >
 
-            </div>
-            <div>
-                <div>
-                    <picture >
-                        <img className=" mx-auto carusel-img img-fluid" src="/uploads/carusel--7.webp" alt="foto-biserica" />
-                    </picture>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <picture >
-                        <img className=" mx-auto carusel-img img-fluid" src="/uploads/carusel_2a.webp" alt="foto-biserica" />
-                    </picture>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <picture >
-                        <img className=" mx-auto carusel-img img-fluid" src="/uploads/carusel_3b.webp" alt="foto-mireasa" />
-                    </picture>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <picture >
-                        <img className=" mx-auto carusel-img img-fluid" src="/uploads/majorat.webp" alt="foto-majorat" />
-                    </picture>
-                </div>
-            </div>
+            <SwiperSlide>
+                <picture className="swiper-zoom-container">
+                    <img className="carusel-img img-fluid" src="/uploads/carusel--2.webp" alt="foto-nunta-natura" />
+                </picture>
 
-        </Slider>
+            </SwiperSlide>
+            <SwiperSlide>
+
+                <picture className="swiper-zoom-container">
+                    <img className="carusel-img img-fluid" src="/uploads/carusel--7.webp" alt="foto-biserica" />
+                </picture>
+            </SwiperSlide>
+            <SwiperSlide>
+
+                <picture className="swiper-zoom-container">
+                    <img className="carusel-img img-fluid" src="/uploads/carusel_2a.webp" alt="foto-biserica" />
+                </picture>
+            </SwiperSlide>
+            <SwiperSlide>
+
+                <picture className="swiper-zoom-container">
+                    <img className="carusel-img img-fluid" src="/uploads/carusel_3b.webp" alt="foto-mireasa" />
+                </picture>
+
+            </SwiperSlide>
+            <SwiperSlide>
+                <picture className="swiper-zoom-container">
+                    <img className="carusel-img img-fluid" src="/uploads/majorat.webp" alt="foto-majorat" />
+                </picture>
+            </SwiperSlide>
+
+        </Swiper>
 
     );
 }
